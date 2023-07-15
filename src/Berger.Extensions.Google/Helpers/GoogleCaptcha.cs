9258ede72f1a0f606c2e5da4160654e01c11f877
Rafael Cronemberger <rafael@berger.com.br>
2023-07-15 16:53:10 -0300

    Packet updates with build yamls

---
+++ src/Berger.Extensions.Google/Helpers/GoogleCaptcha.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace Berger.Extensions.Google
+{
+    public static class GoogleCaptcha
+    {
+        public static bool Check(string secret, string response)
+        {
+            var httpClient = new HttpClient();
+
+            var uri = "https://www.google.com/recaptcha/api/siteverify";
+
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("secret", secret),
+                new KeyValuePair<string, string>("response", response)});
+
+            var result = httpClient.PostAsync(uri, data).Result;
+
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                string content = result.Content.ReadAsStringAsync().Result;
+
+                dynamic confirmation = JObject.Parse(content);
+
+                if (confirmation.success == true)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
